39fa6bfe93a594c580031988fca9ce0895c2536a
amrutapatil27 <143890160+amrutapatil27@users.noreply.github.com>
2026-02-12 22:37:25 +0100

    Initial commit: Playwright automation project setup

---
+++ check_Syastem_Setup.js
@@ -0,0 +1,5 @@
+console.log("Platform: " + process.platform);
+console.log("Node Version: " + process.version);
+console.log("Arhitecture: " + process.arch);
+console.log("Current Working Directory: " + process.cwd());
+console.log("Environment Variables: " + process.env);
